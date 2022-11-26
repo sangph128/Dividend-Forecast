e13a60b162b5cad20d3dd1cd1fe5e3c470e9705b
Sang Pham <wolazm2@gmail.com>
2022-11-26 17:01:51 -0600

    Ticket Symbol

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from datetime import date
+import sys
   
 # Add some color to the window
 sg.theme('BlueMono')
@@ -26,11 +27,43 @@ layout = [
 def main():
     window = sg.Window('Dividend Forecast Calculator', layout)
     event, values = window.read()
-    window.close()
+    window.close() 
 
-    # Returns the current local date
-    today = date.today()
-    print("Today date is: ", today)
+    # ticket symbol
+    if event == 'Submit':
+
+        # Returns the current local date
+        today = date.today()
+        ticket = values[0]
+        stock = yf.Ticker(ticket)
+        
+        # Check invalid ticket symbol
+        if stock.info['regularMarketPrice'] == None:
+            sg.Popup('Ticket Symbol "' + ticket + '" is not listed')
+            sys.exit("Ticket Symbol Not Found")
+        
+        result_table = []
+
+        headings = ['Year', 'Address', 'Phone Number']
+
+        result_layout = [
+                [sg.Table(values=result_table, 
+                headings=headings, 
+                max_col_width=35,
+                            auto_size_columns=True,
+                            justification='right',
+                            key='-TABLE-',
+                            row_height=35)]
+            ]
+
+        w = sg.Window("Dividend Calculator", result_layout)
+        w.read()
+        w.close()
+
+    
+    else:
+        print("Cancel")
+
 
 if __name__ == "__main__":
     main()
